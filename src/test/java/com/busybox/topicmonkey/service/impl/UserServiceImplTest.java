b1384c7dc80e2342a874523cdeae0fa77441c1ec
Vasyl Soltys <vsoltys.dev@gmail.com>
2017-02-15 21:50:30 +0100

    updated test after adding audit support

--- src/test/java/com/busybox/topicmonkey/service/impl/UserServiceImplTest.java
+++ src/test/java/com/busybox/topicmonkey/service/impl/UserServiceImplTest.java
@@ -3,7 +3,6 @@ package com.busybox.topicmonkey.service.impl;
 import com.busybox.topicmonkey.domain.model.User;
 import com.busybox.topicmonkey.domain.repository.UserRepository;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,7 +12,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -36,10 +35,10 @@ public class UserServiceImplTest {
     public void shouldFindByUserId() throws Exception {
         // given
         User user = prepareUser();
-        when(repository.findOne(anyLong())).thenReturn(user);
+        when(repository.findOne(anyString())).thenReturn(user);
 
         // when
-        Optional<User> resultUser = service.findById(anyLong());
+        Optional<User> resultUser = service.findById(anyString());
 
         // then
         assertNotNull(resultUser);
@@ -49,33 +48,31 @@ public class UserServiceImplTest {
     }
 
     @Test
-    @Ignore("enable after updating UserRepository")
     public void shouldFindByLoginName() throws Exception {
         // given
         User user = prepareUser();
-        //when(repository.findByLoginName(user.getLoginName())).thenReturn(user);
+        when(repository.findByLoginName(user.getLoginName())).thenReturn(Optional.of(user));
 
         // when
-        User resultUser = service.findByLoginName(user.getLoginName());
+        Optional<User> resultUser = service.findByLoginName(user.getLoginName());
 
         // then
-        assertEquals(user.getLoginName(), resultUser.getLoginName());
-        assertEquals(user.getEmailAddress(), resultUser.getEmailAddress());
+        assertNotNull(resultUser);
+        assertTrue(resultUser.isPresent());
+        assertEquals(user.getLoginName(), resultUser.get().getLoginName());
+        assertEquals(user.getEmailAddress(), resultUser.get().getEmailAddress());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldFailFindByEmptyLoginName() throws Exception {
         // when
-        User resultUser = service.findByLoginName(null);
+        Optional<User> resultUser = service.findByLoginName(null);
 
         // then
         verifyZeroInteractions(repository);
     }
 
     private User prepareUser() {
-        return User.builder()
-                .withLoginName("test")
-                .withEmailAddress("email@test.com")
-                .build();
+        return User.builder().withLoginName("test").withEmailAddress("email@test.com").build();
     }
 }
